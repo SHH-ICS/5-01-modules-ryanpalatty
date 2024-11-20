58117ddd5422b8f27ba602fc30a488c292202316
ryanpalatty <ryan.palatty@stu.ocsb.ca>
2024-11-20 14:46:43 +0000

    Q1Pythagoras math import

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -1,6 +1,8 @@
 # Create a program that will accept the two legs of a right-angle triangle, a and b, and display the length of the hypotenuse, c. 
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
 
+import math
+
 print("The Hypotenuse of a Trianlge is a² + b² = c². ")
 print("a = one side")
 print("b = other side")

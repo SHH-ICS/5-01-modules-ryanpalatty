9bfc96e23e1067497dc6ba030946ee32c4089b29
ryanpalatty <ryan.palatty@stu.ocsb.ca>
2024-11-20 21:52:28 +0000

    Q2RandomRange Initial

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,13 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+import random
+import math
+
+print("Random Number Picker")
+x=input("Enter 1st number: ")
+x=int(x)
+y=input("Enter 2nd number: ")
+y=int(y)
+z= random.randrange(x,y) 
+print("in between numbers below")
+print("Lucky Number Is: ",z)
